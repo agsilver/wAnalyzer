bf8cf18baceda7e48848b351e6d57f2bb45a3c01
Eyal <eyal.ver2@Gmail.com>
2020-01-29 22:16:40 +0200

    less clutter
    1. no local log file dependency.
    2. back to original parse function.
    3. added sql method to insert specific data.

--- parse.py
+++ parse.py
@@ -1,30 +1,34 @@
 import sqlite3
 from sys import stdout
 
-current_chat = r'C:\Users\eyalv\PycharmProjects\wAnalyzer\chats\sherks.txt'
-errorlog_path = r'C:\Users\eyalv\PycharmProjects\wAnalyzer\log.txt'
+current_chat = r'.\chats\sherks.txt'
 conn = sqlite3.connect("sherks.db")
 c = conn.cursor()
 
 
 #  Database tools
 class Database:
-    @staticmethod  # dump data to the table
+    @staticmethod  # dump entire message data to the table.
     def dump(date_sent, time_sent, author_name, msg_content):
         c.execute("INSERT INTO raw (date, time, author, content) values (?, ?, ?, ?)",
                   (date_sent, time_sent, author_name, msg_content))
         conn.commit()
 
-    @staticmethod  # clear the entire table
+    @staticmethod  # insert data to one column only.
+    def insert(column, text):
+        c.execute("INSERT INTO raw (%s) values (?)" % (column,), (text,))
+        conn.commit()
+
+    @staticmethod  # clear the entire table.
     def clearTable():
         c.execute("DELETE from raw;")
 
 
 #  Open a chat file and an error log file and make sure the encoding is correct.
-def openChat(path, log_path):
-    file = open(path, 'r', encoding='utf-8')
-    log_file = open(log_path, 'w', encoding='utf-8')
-    return file, log_file
+def openChat(path):
+    chat_file = open(path, 'r', encoding='utf-8')
+    log_file = open('errors_log.txt', 'w', encoding='utf-8')
+    return chat_file, log_file
 
 
 # number of lines in chat file.
@@ -42,32 +46,26 @@ def parseChat(row):
     date_sent = ''
     time_sent = ''
     author_name = ''
-    msg_content = ''
     i = 0
-    if row[0].isdigit() is True and row[1].isdigit() is True and row[2] == '/':
-        if row[8] == ",":  # 4-digits day and month
-            date_sent = row[0:8]
-            time_sent = row[10:15]
-            i = 18
-        elif row[7] == ",":  # 3-digits day month
-            date_sent = row[0:7]
-            time_sent = row[9:14]
-            i = 17
-        elif row[6] == ",":  # 2-digits day and month
-            date_sent = row[0:6]
-            time_sent = row[8:13]
-            i = 16
-        # get author's name
-        if ":" in row[i:]:
-            while row[i] != ":":
-                author_name += row[i]
-                i += 1
-        # get message content
-        msg_content = row[i+1:]
-    elif row == '\n':
-        pass
-    else:
-        msg_content = row
+    if row[8] == ",":  # 4-digits day and month
+        date_sent = row[0:8]
+        time_sent = row[10:15]
+        i = 18
+    elif row[7] == ",":  # 3-digits day month
+        date_sent = row[0:7]
+        time_sent = row[9:14]
+        i = 17
+    elif row[6] == ",":  # 2-digits day and month
+        date_sent = row[0:6]
+        time_sent = row[8:13]
+        i = 16
+    # get author's name
+    if ":" in row[i:]:
+        while row[i] != ":":
+            author_name += row[i]
+            i += 1
+    # get message content
+    msg_content = row[i+1:]
     return date_sent, time_sent, author_name, msg_content
 
 
@@ -84,10 +82,10 @@ def progress(part, whole):
 
 
 def main():
-    chat, log = openChat(current_chat, errorlog_path)
+    chat, log = openChat(current_chat)
     tot_lines = chatLength(chat)
     prog = 0
-    chat, log = openChat(current_chat, errorlog_path)
+    chat, log = openChat(current_chat)
     Database.clearTable()
     for line in chat:
         try:
