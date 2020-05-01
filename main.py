21a959e9d2abd428840804bb870e525edda77a83
Eyal <eyal.ver2@Gmail.com>
2020-05-01 15:34:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from uuid import uuid4
 from parse import *
+from tinydb import TinyDB, Query
+
+# SQL
+conn = sqlite3.connect("chat_data.db")
+c = conn.cursor()
+# JSON
+jdb = TinyDB('analysis_db.json')
+qu = Query()
 
 
-def main():
-    chat_name = r".\chats\\" + input("Enter the chat file\'s name to analyze: ") + ".txt"
+def newChat():
+    chat_name = input("Enter the chat file\'s name to analyze: ")
+    chat_path = r".\chats\\" + chat_name + ".txt"
     request_id = str(uuid4())
-    getData(chat_name, request_id)
+    getData(chat_path, request_id)
 
 
 if __name__ == '__main__':
-    main()
+    newChat()
